0ea0f6e8f6ce8365700e109342d4c8b45f7f26a0
Nadeem Sadiq <nadeem.sadiq123@hotmail.com>
2020-01-16 17:33:58 +0000

    further TDD dev around selenuim wrapper

--- src/hot_monkey_engine/browser/seleniumwrapper/selenium_adapter.py
+++ src/hot_monkey_engine/browser/seleniumwrapper/selenium_adapter.py
@@ -1,9 +1,30 @@
 class SeleniumAdapter:
     page = None
 
-    def __init__(self, **kwargs):
-        if len(kwargs) is 0:
-            raise TypeError
+    def __init__(self, selenium):
+        self.selenium = selenium
 
     def goto(self, url):
+        try:
+            self.page = self.selenium.get(url)
+        except:
+            raise BrowserError("Unable to goto page")
+
+    def type(self, characters):
         pass
+
+    def press(self, *args):
+        pass
+
+
+class BrowserError(Exception):
+    pass
+
+from enum import Enum
+
+class Key(Enum):
+    ENTER = 0
+    SHIFT = 1
+    CTRL = 2
+    ESC = 3
+    TAB = 4
--- src/test/seleniumwrapper/test_browser_function.py
+++ src/test/seleniumwrapper/test_browser_function.py
@@ -1,35 +1,48 @@
 import unittest
-from hot_monkey_engine.browser.seleniumwrapper.selenium_adapter import SeleniumAdapter 
+from hot_monkey_engine.browser.seleniumwrapper.selenium_adapter import * 
 
-default_config = {
-    'driver' : 'chrome',
-    'location' : "/path/to/driver",
-}
 
 valid_url = 'https://somewebsite.com'
 test_page = "Some webpage or something"
 
+class SeleniumMock():
+    def get(self, url):
+        return test_page
+
+class SeleniumRaiseErrorMock():
+    def get(self, url):
+        raise OSError
+
+selenium_mock = SeleniumMock()
+selenium_error_mock = SeleniumRaiseErrorMock()
+
 class TestBrowserFunction(unittest.TestCase):
-
-    def test_create_new_browser_with_config(self):
-        adapter = SeleniumAdapter(**default_config)
-        
-        self.assertIsNotNone(adapter, 'An error with creating an adapter')
-
-    def test_errors_when_missing_config(self):
-        with self.assertRaises(TypeError):
-            SeleniumAdapter()
-
-    def test_wrong_driver_setup(self):
-        with self.assertRaises(TypeError):
-            SeleniumAdapter(driver='doesnotexist', location='/does/not/exist')
-
     def test_goto_website(self):
-        adapter = SeleniumAdapter(**default_config)
+        adapter = SeleniumAdapter(selenium_mock)
         
         adapter.goto(valid_url)
 
         self.assertEqual(adapter.page, test_page)
+    
+    def test_error_when_selenium_raises_error(self):
+        with self.assertRaises(BrowserError):
+            adapter = SeleniumAdapter(selenium_error_mock)
+        
+            adapter.goto(valid_url)
+
+    def test_type_to_page(self):
+        adapter = SeleniumAdapter(selenium_error_mock)
+        
+        adapter.type(valid_url)
+
+    def test_type_with_non_character_key_commands_to_page(self):
+        adapter = SeleniumAdapter(selenium_error_mock)
+        
+        adapter.press(Key.ENTER)
+        adapter.press(Key.ESC)
+        adapter.press(Key.TAB)
+        adapter.press(Key.ENTER, Key.CTRL)
+
 
 
 
